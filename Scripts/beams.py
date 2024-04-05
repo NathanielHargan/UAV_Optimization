4db415e8d06bf06c643f156acf07e4992387a23c
Natth4545 <nhargan@vt.edu>
2024-04-05 01:57:32 -0400

    UAV_Mission

--- Scripts/beams.py
+++ Scripts/beams.py
@@ -2,7 +2,6 @@ import numpy as np
 from Scripts.cross_section_properties import cross_section_circle
 from Scripts.cross_section_properties import cross_section_annulus
 
-
 class BeamSystem:
     def __init__(self, name):
         self.system_name = name  # A string with the name of the system
@@ -15,14 +14,11 @@ class BeamSystem:
         self.nodes = np.concatenate((self.nodes, coords), axis=0)
 
         if name is None:
-            node_name = "Node_" + str(len(self.nodes))
+            name = "Node_" + str(len(self.nodes))
 
         self.node_names.append(name)
 
-    def add_beam(self, start_node, end_node, cross_section, cross_section_parameters, material, name=None):
-        start_node_coords = [0, 0, 0]
-        end_node_coords = [0, 0, 0]
-
+    def add_beam(self, beam_type, start_node, end_node, name=None):
         if type(start_node) is int:
             start_node_coords = self.nodes[start_node]
             end_node_coords = self.nodes[end_node]
@@ -31,6 +27,8 @@ class BeamSystem:
             start_node_coords = self.nodes[self.node_names.index(start_node)]
             end_node_coords = self.nodes[self.node_names.index(end_node)]
         else:
+            start_node_coords = [0, 0, 0]
+            end_node_coords = [0, 0, 0]
             print("start or end nodes must be string or integer")
 
         if name is None:
@@ -38,30 +36,33 @@ class BeamSystem:
 
         self.beams.append(
             Beam(name,
+                 beam_type,
                  start_node_coords,
-                 end_node_coords,
-                 cross_section,
-                 cross_section_parameters,
-                 material))
+                 end_node_coords))
 
 
 class Beam:
-    def __init__(self, name, start_node_coords, end_node_coords, cross_section, cross_section_parameters, material):
+    def __init__(self, beam_type, start_node_coords, end_node_coords, name=None):
         self.name = name  # String with the beam name
-        self.cross_section = cross_section  # Type of cross-section "Circle" or "Annulus"
-
-        # [r1] for circle [r1,r2] for annulus
-        self.cross_section_parameters = cross_section_parameters
-
-        # Will add this soon. I do not know how I will structure this data. I will figure it out.
-        self.material = material
+        self.beam_type = beam_type
 
         # Coordinates of the nodes
         self.start_node_coords = start_node_coords
         self.end_node_coords = end_node_coords
 
-        # Empty dictionary which is filled with the
-        self.cross_section_properties = {}
+
+class BeamType:
+    def __init__(self, cross_section, cross_section_parameters, material, name=None):
+        self.cross_section = cross_section
+        self.cross_section_parameters = cross_section_parameters
+        self.material = material
+
+        # Name is optional so
+        if name is None:
+            self.name = "Beam_Type_" + cross_section + "_" + material
+        else:
+            self.name = name
+
         self.cross_section_properties = self.cross_section_properties_init()
 
     def cross_section_properties_init(self):
@@ -71,3 +72,4 @@ class Beam:
             return cross_section_annulus(self.cross_section_parameters)
         else:
             print('incorrect cross section in beam ' + self.name)
+            return 'error'
--- Scripts/drone.py
+++ Scripts/drone.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
 from Scripts.beams import BeamSystem
+from Scripts.beams import BeamType
 
 
 class Drone:
@@ -9,12 +10,12 @@ class Drone:
         self.beam_system = BeamSystem(self.name + "_system")
 
     def create_drone(self, hub_rad, nominal_rad, strut_pos, blade_num):
-        strut_rad_para = [0.3, 0.25]
-        arm_rad_para = [0.5, 0.45]
-        hub_rad_para = [1, 0.5]
+        arm_beam = BeamType("annulus",[0.5, 0.45],"material xyz","arm_beam")
+        strut_beam = BeamType("annulus",[0.3, 0.25],"material xyz","arm_beam")
+        hub_beam = BeamType("annulus",[1, 0.5],"material xyz","arm_beam")
 
         num = 0
-        # Rotational Symmetry
+        # Rotational Symmetry Nodes
         for theta in np.linspace(0, 2*math.pi*(blade_num-1)/blade_num, num=blade_num):
             # Adds the node where the blades are
             self.beam_system.add_node(
@@ -39,54 +40,43 @@ class Drone:
 
             num += 1
 
+        # Adding Beams
         for i in range(0, blade_num):
             # Outer to Strut
             self.beam_system.add_beam(
+                arm_beam,
                 "outer_node_" + str(i),
-                "strut_node_" + str(i),
-                "annulus",
-                arm_rad_para,
-                "carbon fiber XYZ")
+                "strut_node_" + str(i))
 
             # Strut to Hub
             self.beam_system.add_beam(
+                arm_beam,
                 "strut_node_" + str(i),
-                "hub_node_" + str(i),
-                "annulus",
-                arm_rad_para,
-                "carbon fiber XYZ")
+                "hub_node_" + str(i))
 
         # Final to 0
         self.beam_system.add_beam(
+            strut_beam,
             "strut_node_" + str(blade_num-1),
-            "strut_node_0",
-            "annulus",
-            strut_rad_para,
-            "carbon fiber XYZ")
+            "strut_node_0")
 
         self.beam_system.add_beam(
+            hub_beam,
             "hub_node_" + str(blade_num-1),
-            "hub_node_0",
-            "annulus",
-            hub_rad_para,
-            "carbon fiber XYZ")
+            "hub_node_0")
 
         for i in range(0, blade_num-1):
             # Strut to Strut
             self.beam_system.add_beam(
+                strut_beam,
                 "strut_node_" + str(i),
-                "strut_node_" + str(i+1),
-                "annulus",
-                strut_rad_para,
-                "carbon fiber XYZ")
+                "strut_node_" + str(i+1))
 
             # Hub to Hub
             self.beam_system.add_beam(
+                hub_beam,
                 "hub_node_" + str(i),
-                "hub_node_" + str(i+1),
-                "annulus",
-                hub_rad_para,
-                "carbon fiber XYZ")
+                "hub_node_" + str(i+1))
 
 
 
