4e2e0c6812d8e608b10b707c4f502799b8eecebd
Natth4545 <nhargan@vt.edu>
2024-09-24 03:35:05 -0400

    more stuff

--- Scripts/DroneMass.py
+++ Scripts/DroneMass.py
@@ -28,13 +28,23 @@ class DroneMass:
         self.arm_mass = arm_linear_density * self.drone.nominal_rad
         self.strut_mass = strut_linear_density * self.drone.strut_length
 
+        self.arm_ixx = self.drone.arm_beam
+
         self.lumped_mass_frame = self.drone.blade_num * (self.arm_mass + self.strut_mass)
 
         self.arm_beam_centroids = np.zeros([self.drone.blade_num, 3])
         self.strut_beam_centroids = np.zeros([self.drone.blade_num, 3])
+        self.battery_centroids = np.zeros([len(self.batteries), 3])
         self.centroid_calculation()
-        print(self.lumped_mass_frame)
-        print(self.strut_beam_centroids)
+
+        self.total_mass = self.lumped_mass_frame
+
+        self.total_mass_calculation() # add battery mass
+
+        self.centroids = np.empty([])
+        self.masses = np.empty([])
+        self.center_of_mass = np.zeros(3)
+        self.center_of_mass_calcuation()
 
     def centroid_calculation(self):
         for i in range(self.drone.blade_num):
@@ -46,6 +56,33 @@ class DroneMass:
             self.strut_beam_centroids[i][0] = math.cos(theta + math.pi / self.drone.blade_num) * self.drone.strut_pos
             self.strut_beam_centroids[i][1] = math.sin(theta + math.pi / self.drone.blade_num) * self.drone.strut_pos
             self.strut_beam_centroids[i][2] = 0
+
+        for i, battery in enumerate(self.batteries):
+            self.battery_centroids[i] = battery.position
+
+
+    def total_mass_calculation(self):
+        for battery in self.batteries:
+            self.total_mass += battery.mass
+
+    def center_of_mass_calcuation(self):
+        self.centroids = np.transpose(np.vstack([self.arm_beam_centroids, self.strut_beam_centroids, self.battery_centroids]))
+        arm_beam_masses = np.zeros([self.drone.blade_num])
+        strut_beam_masses = np.zeros([self.drone.blade_num])
+        battery_masses = np.zeros([len(self.batteries)])
+
+        for i in range(self.drone.blade_num):
+            arm_beam_masses[i] = self.arm_mass
+            strut_beam_masses[i] = self.strut_mass
+
+        for i, battery in enumerate(self.batteries):
+            battery_masses[i] = battery.mass
+
+        self.masses = np.transpose(np.hstack([arm_beam_masses, strut_beam_masses, battery_masses]))
+        self.center_of_mass = (self.centroids @ self.masses) / self.total_mass
+        print(self.center_of_mass)
+
+
 
 
 if __name__ == '__main__':
